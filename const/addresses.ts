6a76411715a75ab06c756f1cb4115bfde5106259
snowland <snowlandvault@gmail.com>
2023-08-10 16:25:34 +0200

    first commit

---
+++ const/addresses.ts
@@ -0,0 +1,3 @@
+export const LOTTERY_CONTRACT_ADDRESS = '0x39E84AfBCB4CBD9bf53c33657f3a356bD6e2c405';
+
+export const HERO_IMAGE_URL = 'https://user-images.githubusercontent.com/14011726/94132137-7d4fc100-fe7c-11ea-8512-69f90cb65e48.gif';
---
+++ pages/index.tsx
@@ -0,0 +1,123 @@
+import type { NextPage } from "next";
+import { Box, Button, Container, Flex, Input, SimpleGrid, Stack, Text } from "@chakra-ui/react";
+import { MediaRenderer, Web3Button, useAddress, useContract, useContractRead } from "@thirdweb-dev/react";
+import { HERO_IMAGE_URL, LOTTERY_CONTRACT_ADDRESS } from "../const/addresses";
+import LotteryStatus from "../components/Status";
+import { ethers } from "ethers";
+import PrizeNFT from "../components/PrizeNFT";
+import { useState } from "react";
+import CurrentEntries from "../components/CurrentEntries";
+
+const Home: NextPage = () => {
+  const address = useAddress();
+
+  const {
+    contract
+  } = useContract(LOTTERY_CONTRACT_ADDRESS);
+
+  const {
+    data: lotteryStatus
+  } = useContractRead(contract, "lotteryStatus");
+
+  const {
+    data: ticketCost,
+    isLoading: ticketCostLoading
+  } = useContractRead(contract, "ticketCost");
+  const ticketCostInEther = ticketCost ? ethers.utils.formatEther(ticketCost) : "0";
+
+  const {
+    data: totalEntries,
+    isLoading: totalEntriesLoading
+  } = useContractRead(contract, "totalEntries");
+
+  const [ticketAmount, setTicketAmount] = useState(0);
+  const ticketCostSubmit = parseFloat(ticketCostInEther) * ticketAmount;
+
+  function increaseTicketAmount() {
+    setTicketAmount(ticketAmount + 1);
+  }
+
+  function decreaseTicketAmount() {
+    if (ticketAmount > 0) {
+      setTicketAmount(ticketAmount - 1);
+    }
+  }
+
+  return (
+    <Container maxW={"1440px"}>
+      <SimpleGrid columns={2} spacing={4} minH={"60vh"}>
+        <Flex justifyContent={"center"} alignItems={"center"}>
+          {lotteryStatus ? (
+            <PrizeNFT/>
+          ) : (
+            <MediaRenderer
+              src={HERO_IMAGE_URL}
+              width="100%"
+              height="100%"
+            />
+          )}
+          
+        </Flex>
+        <Flex justifyContent={"center"} alignItems={"center"} p={"5%"}>
+          <Stack spacing={10}>
+            <Box>
+              <Text fontSize={"xl"}>Raffle App</Text>
+              <Text fontSize={"4xl"} fontWeight={"bold"}>티켓을 구매해 NFT 상품을 획득하세요!</Text>
+            </Box>
+            
+            <Text fontSize={"xl"}>응모권을 구매해 NFT를 받을 수 있는 기회를 잡으세요! 당첨자가 선정되어 NFT가 전송됩니다. 응모권이 많을수록 당첨 확률이 높아집니다.</Text>
+            
+            <LotteryStatus status={lotteryStatus}/>
+            {!ticketCostLoading && (
+              <Text fontSize={"2xl"} fontWeight={"bold"}>티켓당 가격: {ticketCostInEther} MATIC</Text>
+            )}
+            {address ? (
+              <Flex flexDirection={"row"}>
+                <Flex flexDirection={"row"} w={"25%"} mr={"40px"}>
+                  <Button
+                    onClick={decreaseTicketAmount}
+                  >-</Button>
+                  <Input
+                    value={ticketAmount}
+                    type={"number"}
+                    onChange={(e) => setTicketAmount(parseInt(e.target.value))}
+                    textAlign={"center"}
+                    mx={2}
+                  />
+                  <Button
+                    onClick={increaseTicketAmount}
+                  >+</Button>
+                </Flex>
+                
+                <Web3Button
+                  contractAddress={LOTTERY_CONTRACT_ADDRESS}
+                  action={(contract) => contract.call(
+                    "buyTicket",
+                    [
+                      ticketAmount
+                    ],
+                    {
+                      value: ethers.utils.parseEther(ticketCostSubmit.toString())
+                    }
+                  )}
+                  isDisabled={!lotteryStatus}
+                >{`Buy Ticket(s)`}</Web3Button>
+              </Flex>
+            ) : (
+              <Text>Connect wallet to buy ticket.</Text>
+            )}
+            {!totalEntriesLoading && (
+              <Text>총 응모권: {totalEntries.toString()}</Text>
+            )}
+          </Stack>
+        </Flex>
+      </SimpleGrid>
+      <Stack mt={"40px"} textAlign={"center"}>
+        <Text fontSize={"xl"}>현재 추첨 참여자 수:</Text>
+        <CurrentEntries/>
+      </Stack>
+    </Container>
+  );
+};
+
+export default Home;
