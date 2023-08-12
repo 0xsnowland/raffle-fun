f2cec39452d506264cd86dd8584c8a4cb19ed6c9
snowland <snowlandvault@gmail.com>
2023-08-12 12:52:55 +0200

    first commit

--- const/addresses.ts
+++ const/addresses.ts
@@ -1,3 +1,3 @@
 export const LOTTERY_CONTRACT_ADDRESS = '0x9EF60d8dfb2e29FAC79007379e68d7d0F98ccD33';
 
-export const HERO_IMAGE_URL = 'https://15065ae3c21e0bff07eaf80b713a6ef0.ipfscdn.io/ipfs/bafybeienas2qtlisiyydvuggccntiurecvbbuqpw5jqt3jct3ee2rm6npq/Screenshot%202023-08-10%20at%2016.55.55.jpg';
+export const HERO_IMAGE_URL = 'https://15065ae3c21e0bff07eaf80b713a6ef0.ipfscdn.io/ipfs/bafybeicwzwpiwr3w2e7siwoimp76ggfvoegua4afd6zgawbeyvjy7cunou/1.png';
